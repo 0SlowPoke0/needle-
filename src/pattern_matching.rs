3fe6164c3465bd1264102ad8d258fd5fa08951c8
0SlowPoke0 <govindpratyush@gmail.com>
2025-09-12 13:17:10 +0530

    added negative pattern matching

--- src/pattern_matching.rs
+++ src/pattern_matching.rs
@@ -1,29 +1,39 @@
+use std::collections::HashSet;
+
 pub fn match_pattern(input_line: &str, pattern: &str) -> bool {
     match pattern {
         p if p.starts_with(r"\d") => input_line.chars().any(|c| c.is_ascii_digit()),
         p if p.starts_with(r"\w") => input_line.chars().any(|c| c.is_alphanumeric() || c == '_'),
-        p if p.starts_with("[") => check_positive_character_groups(input_line, pattern),
+        p if p.starts_with("[^") => check_character_groups(input_line, pattern, false),
+        p if p.starts_with("[") => check_character_groups(input_line, pattern, true),
         p if p.chars().count() == 1 => input_line.contains(pattern),
         _ => panic!("Unhandled pattern: {}", pattern),
     }
 }
 
-fn check_positive_character_groups(input_line: &str, pattern: &str) -> bool {
+fn check_character_groups(input_line: &str, pattern: &str, positive: bool) -> bool {
+    let prefix = if positive { "[" } else { "[^" };
     let Some(target) = pattern
-        .strip_prefix('[')
+        .strip_prefix(prefix)
         .map(|c| c.strip_suffix(']'))
         .flatten()
     else {
         return false;
     };
 
-    let check_word_contains_target = |c: &str| -> bool {
-        let target_chars = target.chars().collect::<Vec<_>>();
+    let check_word_contains_target = |c: &str, positive: bool| -> bool {
+        let target_chars = target.chars().collect::<HashSet<_>>();
 
-        c.chars()
-            .any(|word_element| target_chars.contains(&word_element))
+        if positive {
+            c.chars()
+                .any(|word_element| target_chars.contains(&word_element))
+        } else {
+            c.chars()
+                .any(|word_element| !target_chars.contains(&word_element))
+        }
     };
+
     input_line
         .split_whitespace()
-        .any(|c| check_word_contains_target(c))
+        .any(|c| check_word_contains_target(c, positive))
 }
