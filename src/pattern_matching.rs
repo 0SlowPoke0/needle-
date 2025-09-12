c7466abd28d13d8caadcee6a345e44f96f444b57
0SlowPoke0 <govindpratyush@gmail.com>
2025-09-12 11:24:47 +0530

    matched words

--- src/pattern_matching.rs
+++ src/pattern_matching.rs
@@ -1,6 +1,7 @@
 pub fn match_pattern(input_line: &str, pattern: &str) -> bool {
     match pattern {
         p if p.starts_with(r"\d") => input_line.chars().any(|c| c.is_ascii_digit()),
+        p if p.starts_with(r"\w") => input_line.chars().any(|c| c.is_alphanumeric() || c == '_'),
         p if p.chars().count() == 1 => input_line.contains(pattern),
         _ => panic!("Unhandled pattern: {}", pattern),
     }
