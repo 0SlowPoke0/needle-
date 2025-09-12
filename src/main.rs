2db81e1844388300260213fd23803f32bf47fc0d
0SlowPoke0 <govindpratyush@gmail.com>
2025-09-12 11:21:20 +0530

    matched digits

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,28 @@
 use std::env;
 use std::io;
 use std::process;
-
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
-    }
-}
+mod pattern_matching;
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
+    // eprintln!("Logs from your program will appear here!");
 
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
     }
 
-    let pattern = env::args().nth(2).unwrap();
+    let Some(pattern) = env::args().nth(2) else {
+        eprintln!("No pattern to check was provided");
+        process::exit(1)
+    };
     let mut input_line = String::new();
 
     io::stdin().read_line(&mut input_line).unwrap();
 
     // Uncomment this block to pass the first stage
-    if match_pattern(&input_line, &pattern) {
+    if pattern_matching::match_pattern(&input_line, &pattern) {
         process::exit(0)
     } else {
         process::exit(1)
---
+++ src/pattern_matching.rs
@@ -0,0 +1,7 @@
+pub fn match_pattern(input_line: &str, pattern: &str) -> bool {
+    match pattern {
+        p if p.starts_with(r"\d") => input_line.chars().any(|c| c.is_ascii_digit()),
+        p if p.chars().count() == 1 => input_line.contains(pattern),
+        _ => panic!("Unhandled pattern: {}", pattern),
+    }
+}
